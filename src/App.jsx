109f0e9ab79f0b9003be30c01eec86d9afa89b10
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2025-03-28 12:09:22 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MandelbrotLogo from './assets/mandelbrot.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <img src={MandelbrotLogo} className="logo" alt="Mandelbrot Logo" />
+      <h1>Mandelbrot</h1>
+    </>
+  )
+}
+
+export default App;
